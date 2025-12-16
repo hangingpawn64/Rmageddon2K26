e4020fba442e2d5cced28b55e3faac6dc8868286
swarangi-mukwane <https://github.com/swarangi-mukwane/javascript.git>
2025-12-16 14:46:43 +0530

    add accomodation page

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div>
+      <div className="bg">
+       <div className="overlay"></div>  
+         <div className="accomodation">ACCOMODATION</div>
+           <div className="soon">COMING SOON</div>
+        <div className="left"> </div>
+       <div className="right"> </div>
+      </div>
+    </div>
+  );
+}
