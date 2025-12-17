664fa7b556518bed1e8ae1d4a9d72677dd2da7ac
Rugved <rugvedsitafale1014@gmail.com>
2025-12-18 01:39:45 +0530

    SVGs added

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,39 +1,37 @@
 import "./Hero.css";
 import bgImg from "../assets/bg.png";
 import gtaArt from "../assets/char.png";
-import Logo from "./Logo";
-import StepCard from "./StepCard"; // ✅ correct casing
+import leftHUD from "../assets/Group1.svg";
+import rightHUD from "../assets/Group2.svg";
+import StepCard from "./StepCard";
 import { steps } from "./StepsData";
-import Navbar from "./Navbar"
 
 export default function Hero() {
   return (
-    
     <section
       className="hero"
       style={{ backgroundImage: `url(${bgImg})` }}
     >
-      <Navbar/>
-      {/* TOP-RIGHT LOGO */}
-      {/* <Logo /> */}
-
       {/* HUD TITLE */}
- <h1 className="hud-title">
-  <span className="hud-line-1">
-    Steps to confirm your seat in
-  </span>
+    {/* HUD TITLE BLOCK */}
+<div className="hud-container">
+  <img src={leftHUD} alt="" className="hud-svg hud-svg-left" />
 
-  <span className="hud-line-2">
-    RMAGEDDON 26
-    <span className="hud-connector"></span>
-  </span>
-</h1>
+  <h1 className="hud-title">
+    <span className="hud-line-1">
+      Steps to confirm your seat in
+    </span>
+    <span className="hud-line-2">
+      RMAGEDDON 26
+    </span>
+  </h1>
 
+  <img src={rightHUD} alt="" className="hud-svg hud-svg-right" />
+</div>
 
 
       {/* MAIN CONTENT */}
       <div className="hero__overlay">
-        {/* LEFT SIDE – CARDS */}
         <div className="hero__left">
           <div className="hero__cards">
             {steps.map((step) => (
@@ -42,11 +40,10 @@ export default function Hero() {
           </div>
         </div>
 
-        {/* RIGHT SIDE – CHARACTER */}
         <div className="hero__right">
           <img
             src={gtaArt}
-            alt="RMAGEDDON Character"
+            alt="RMAGEDDON character"
             className="hero__art"
           />
         </div>
