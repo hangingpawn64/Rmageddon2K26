d619513c17d256f0dec34ce84f160e62920908a5
swarangi-mukwane <https://github.com/swarangi-mukwane/javascript.git>
2025-12-23 16:09:02 +0530

    changes in home_page3

--- src/components/Home_page3.jsx
+++ src/components/Home_page3.jsx
@@ -1,29 +1,84 @@
 import React from "react";
 import "./Home_page3.css";
+
 export default function Home_page3() {
   return (
     <div className="home3-bg">
-      <div className="home3-overlay"></div> 
-      <h1 className="title" > Discover Your Potential with Rmageddon</h1>
-      <div className="textbg">
-      
-        
+      <div className="home3-overlay">
+        <div className="title">
+          Discover Your Potential with Rmageddon
+        </div>
+
+        <div className="container">
+          <div className="leftside">
+            <div className="textbg">
+              <img
+                src="https://res.cloudinary.com/dmbmdfmhl/image/upload/v1766336885/Group_514617188_pvktop.png"
+                alt="Rmageddon"
+              />
+            </div>
+
+            <div className="content">
+              <ul>
+                <li>
+                  Development of teamwork and learn "GAMESMANSHIP" and
+                  "CREATIVITY" - a crucial 21st century skills.
+                </li>
+                <li>
+                  Exceptional hands-on learning experience with designing,
+                  building and complement your engineering curriculum.
+                </li>
+                <li>
+                  Exciting Prizes,Certificates after completion and a ton of
+                  excitement.
+                </li>
+                <li>
+                  By participating in the competition, familiarize yourself
+                  with SCI-FI realm.
+                </li>
+              </ul>
+            </div>
+
+            <div className="leftimg">
+              <img
+                src="https://res.cloudinary.com/dmbmdfmhl/image/upload/v1766339444/31-312191_grand-theft-auto-v-game-free-png-gta_2_urzmnt.png"
+                alt=""
+              />
+            </div>
+          </div>
+
+          <div className="rightside">
+            <div className="image">
+              <img
+                src="https://res.cloudinary.com/dmbmdfmhl/image/upload/v1766337076/Group_514617161_xwk6xm.png"
+                alt=""
+              />
+            </div>
+
+            <div className="rightimg">
+              <img
+                src="https://res.cloudinary.com/dmbmdfmhl/image/upload/v1766338254/Group_514617172_fkwsyk.png"
+                alt=""
+              />
+            </div>
+
+            <div className="innertext">
+              CERTIFICATE OF PARTICIPATION
+            </div>
+
+            <div className="rightimg2">
+              <img
+                src="https://res.cloudinary.com/dmbmdfmhl/image/upload/v1766338254/Group_514617172_fkwsyk.png"
+                alt=""
+              />
+            </div>
+
+            <div className="innertext2">
+              CASH PRIZES
+            </div>
+          </div>
+        </div>
       </div>
-       <div className="content">
-        <li>Development of teamwork and learn "GAMESMANSHIP" and "CREATIVITY" - a crucial 21st century skills.</li>
-        <li>Exceptional hands-on learning experience with designing, building and complement your engineering curriculum.</li>
-        <li>Exciting Prizes,Certificates after completion and a ton of excitement.</li>
-        <li>By participating in the competition, familiarize yourself with SCI-FI realm.</li>
-      </div> 
-      <div className="leftimg"> </div>
-      <div className="rightimg"></div>
-        <div className="innertext">CERTIFICATE OF PARTICIPATION</div>
-      
-      <div className="rightimg2"></div>
-        <div className="innertext2">CASH PRIZES</div>
-      
-      <div className="image"></div>
     </div>
-    );
+  );
 }
- 
