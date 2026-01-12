49eca6341e3a4c3e067d038fe30a338b219e49c7
hangingpawn64 <2024bcs156@sggs.ac.in>
2026-01-12 20:11:00 +0530

    cards back fix, homepage three button animation fix

--- src/pages/Homepage/Home_page3.jsx
+++ src/pages/Homepage/Home_page3.jsx
@@ -47,7 +47,7 @@ export default function Home_page3() {
             </div>
 
             {/* MAIN IMAGE */}
-            <div className="home3-leftimg" data-aos="fade-up" data-aos-delay="1500">
+            <div className="home3-leftimg" data-aos="fade-up" data-aos-delay="1200">
               <img
                 src="https://res.cloudinary.com/dmbmdfmhl/image/upload/v1766339444/31-312191_grand-theft-auto-v-game-free-png-gta_2_urzmnt.png"
                 alt="GTA"
@@ -58,7 +58,7 @@ export default function Home_page3() {
 
 
           <div className="home3-rightside">
-            <div className="home3-right-up" data-aos="fade-down" data-aos-delay="1500">
+            <div className="home3-right-up" data-aos="fade-down" data-aos-delay="800">
               <img
                 src="https://res.cloudinary.com/dmbmdfmhl/image/upload/v1766933652/ChatGPT_Image_Dec_28_2025_07_01_20_PM_1_u3hcr7.png"
                 alt=""
@@ -66,7 +66,7 @@ export default function Home_page3() {
             </div>
 
 
-            <div className="home3-right-low" data-aos="fade-left" data-aos-delay="1500">
+            <div className="home3-right-low" data-aos="fade-left" data-aos-delay="1000">
               {/* CERTIFICATE */}
               <div className="home3-rightimg1">
                 <img
